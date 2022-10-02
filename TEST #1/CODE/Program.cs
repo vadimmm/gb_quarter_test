49d3a5a513f93e8a180ff624dc6cdf20d22a371b
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-10-02 16:59:59 +0300

    test 01: code logic ok

---
+++ TEST #1/CODE/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] arrayInput = new string[] {"hello", "2", "world", ":-)"};
+string[] arrayResult = new string[arrayInput.GetLength(0)];
+int ControlLength = 3;
+
+void DisplayStringArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i] != null)
+        {
+            Console.WriteLine($"{array[i]}");
+        }
+    }
+}
+
+Console.WriteLine("\nВводные данные для решения задачи:");
+DisplayStringArray(arrayInput);
+
+int count = 0;
+for (int i = 0; i < arrayInput.GetLength(0); i++)
+{
+    if (arrayInput[i].Length <= ControlLength)
+    {
+        arrayResult[count] = arrayInput[i];
+        count++;
+    }
+    else
+    {
+        continue;
+    }
+}
+
+Console.WriteLine("\nОТВЕТ: Значения добавленные во второй массив:");
+DisplayStringArray(arrayResult);
+
+// for (int i = 0; i < arrayResult.GetLength(0); i++)
+// {
+//     if (arrayResult[i] != null)
+//     {
+//         Console.WriteLine($"{arrayResult[i]}");
+//     }
+// }
